f2505e2df9642732de4be3f94d24880da44e4610
RAJU <pydiraju4262@gmail.com>
2024-09-02 13:17:32 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 7000;
 
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'https://first-website-backend.onrender.com'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
